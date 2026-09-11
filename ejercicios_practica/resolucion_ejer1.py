93838cb7c0807951e51af307d71b48fde7b2cb2e
Alessandra Balconi <dabalconi@correo.url.edu.gt>
2026-09-11 23:15:33 +0000

    configurando git para commits remoto

---
+++ ejercicios_practica/resolucion_ejer1.py
@@ -0,0 +1,6 @@
+print("Hola mundo desde el dispositivo de Diana")
+
+# Código creado para obtener la IP del dispositivo
+import platform
+plataforma = platform.system()
+print("La platafroma del dispositivo es:", plataforma)
